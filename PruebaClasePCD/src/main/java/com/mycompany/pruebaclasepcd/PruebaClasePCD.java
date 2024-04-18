ee5e447e90be05a10d34d11d89e3610f91910f95
mariaeslavagutierrez <eslava.146109@e.unavarra.es>
2024-04-18 16:19:14 +0200

    esto es una prueba de commit más

---
+++ PruebaClasePCD/src/main/java/com/mycompany/pruebaclasepcd/PruebaClasePCD.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebaclasepcd;
+
+/**
+ *
+ * @author alumno
+ */
+public class PruebaClasePCD {
+
+    public static void main(String[] args) {
+        System.out.println("He intentado hacer algo en github");
+    }
+}
